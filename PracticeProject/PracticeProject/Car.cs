46c153da52eba13eab07ee75d000e435ddfe6f90
Fue Moua <MadisonLifeAgent@gmail.com>
2021-08-25 12:35:16 -0500

    added car class and went through class and debugging demos

---
+++ PracticeProject/PracticeProject/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeProject
+{
+    class Car
+    {
+        // member variables for car class
+        public string make;
+        public string model;
+        public string color;
+        public int year;
+        public bool isConvertible;
+
+        // constructor
+        public Car(string make, string model, string color, int year)
+        {
+            this.make = make;
+            this.model = model;
+            this.color = color;
+            this.year = year;
+
+            isConvertible = false;
+        }
+
+        public void DisplayCarInformation()
+        {
+            Console.WriteLine("This car is a " + make + " " + model + ".");
+            Console.WriteLine("It is a " + color + " a car from the year " + year + ".");
+        }
+
+        // member methods
+        public void GiveCarPaintJob(string newColor)
+        {
+            color = newColor;
+        }
+    }
+}
--- PracticeProject/PracticeProject/Program.cs
+++ PracticeProject/PracticeProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// this file is built-in, this is where you run your application/program
+using System;
 
 namespace PracticeProject
 {
@@ -6,7 +7,37 @@ namespace PracticeProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // insert code to run your program here
+
+            // Car is the data type (the class), NissanMaxima is the variable (or object), it is equal to a new Car() class
+            // Car NissanMaxima = new Car();
+            Car NissanMaxima = new Car("Nissan", "Maxima", "Navy", 2013);
+
+            // set the values for your object using dot notation to access the attributes
+/*          NissanMaxima.color = "Navy";
+            NissanMaxima.make = "Nissan";
+            NissanMaxima.model = "Maxima";
+            NissanMaxima.year = 2013;
+            NissanMaxima.isConvertible = false;
+*/
+
+            // set the values for an object using the constructor in its class model
+            Car ToyotaRav4 = new Car("Toyota", "Rav4", "Silver", 2015);
+
+            // Print to the console/terminal using Console.WriteLine or calling a method that has a print function
+            Console.WriteLine(NissanMaxima.make + " " + NissanMaxima.model);
+            NissanMaxima.DisplayCarInformation();
+            ToyotaRav4.DisplayCarInformation();
+
+            // change the paint color of the car
+            Console.WriteLine("Please enter a new color for the Toyota: ");
+            ToyotaRav4.GiveCarPaintJob(Console.ReadLine());
+
+            // Display toyota with new color
+            ToyotaRav4.DisplayCarInformation();
+
+            // Keeps Terminal Open
+            Console.ReadLine();
         }
     }
 }
